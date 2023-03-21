dfb8ab5f7e2ef8620fd69614f89ff95f87bf41f0
André de O Pires <deoliveirapires.andre@gmail.com>
2023-03-21 09:35:07 -0300

    First class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import bordgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ois");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
