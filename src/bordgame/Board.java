4eff5bacc522560a3e8c291d14ae561c5bedc5a2
Andre <deoliveirapires.andre@gmail.com>
2024-02-22 19:30:21 -0300

    Implementing possible moves of King

--- src/bordgame/Board.java
+++ src/bordgame/Board.java
@@ -42,6 +42,7 @@ public class Board {
 			throw new BoardException("There is already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 	
 	
